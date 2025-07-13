a3b10a078db53883045d3a1aa14ebe2a5fff136a
mrasyid-ridha <mrasyidridha005@gmail.com>
2025-07-13 16:45:05 +0800

    Project PBO 1

---
+++ MenuItem.java
@@ -0,0 +1,17 @@
+public class MenuItem {
+    private String nama;
+    private double harga;
+
+    public MenuItem(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+}
